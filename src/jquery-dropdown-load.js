301dff4effafc53e952d5faf4823f606fef268c5
mike <memecache@qq.com>
2015-04-03 11:34:07 +0800

    下拉方式优化

--- src/jquery-dropdown-load.js
+++ src/jquery-dropdown-load.js
@@ -1,34 +1,42 @@
 $.fn.extend({
 	autoload : function(url, before, callback, end) {
 		var target = $(this);
-		$(window).scroll(
-				function() {
-					if (($(document).height() - $(this).scrollTop() - $(this)
-							.height()) < 50) {
-						var loadTotal = target.children().length;
-						$.ajax({
-							type : 'get',
-							data : {
-								act : 'loadmore',
-								count : loadTotal
-							},
-							url : url,
-							context : target,
-							async : false,
-							beforeSend : function() {
-								before();
-							},
-							success : function(o) {
-								if (!o || o.length < 2) {
-									window.onscroll = null;
-									end();
-									return;
-								}
-								$(this).append(o);
-								callback(o);
-							}
-						});
+		var allowFlag = true;
+		
+		function load() {
+			var loadTotal = target.children().length;
+			$.ajax({
+				type : 'get',
+				data : {
+					act : 'loadmore',
+					count : loadTotal
+				},
+				url : url,
+				context : target,
+				//async : false,
+				beforeSend : function() {
+					before();
+				},
+				success : function(o) {
+					if (!o || o.length < 2) {
+						window.onscroll = null;
+						end();
+						return;
 					}
-				});
+					setTimeout(function(){
+						allowFlag = true;
+						target.append(o);
+						callback(o);
+					},1000);
+				}
+			});
+		}
+
+		$(window).scroll(function(){
+			if (allowFlag &&($(document).height() - $(this).scrollTop() - $(this).height()) < 1) {		
+				allowFlag = false;
+				load();
+			}
+		});
 	}
 });
